182f616641dc615f2dbb588bbbb89009acc7ba66
gxs114 <usagisah@163.com>
2022-05-20 00:05:21 +0800

    优化方法

--- lib/useDebounce.ts
+++ lib/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useEffect, useLayoutEffect, useRef } from "react"
 
 /**
- * @description 防抖函数，内部使用 useEffect 监听，没有闭包陷阱
+ * @description 防抖函数，只有间隔指定时间才会执行，内部使用 useEffect 监听，没有闭包陷阱
  * @param fn 要执行的函数
  * @param deeps 监听的值的依赖数组
  * @param delay 防抖时间，默认是 700ms
@@ -16,7 +16,7 @@ export function useDebounce(fn: () => void, deeps: unknown[], delay = 700) {
   const timer = useRef<number>()
   useEffect(() => {
     clearTimeout(timer.current)
-    timer.current = setTimeout(fn, delay)
+    timer.current = setTimeout(fn, delay) as any
   }, deeps)
 }
 
@@ -40,6 +40,6 @@ export function useLayoutDebounce(
   const timer = useRef<number>()
   useLayoutEffect(() => {
     clearTimeout(timer.current)
-    timer.current = setTimeout(fn, delay)
+    timer.current = setTimeout(fn, delay) as any
   }, deeps)
 }
